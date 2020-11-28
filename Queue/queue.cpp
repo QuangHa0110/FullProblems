594269671c5b71af52d75e5264fd910abe81adc0
Quang Ha <quanghadang08@gmail.com>
2020-11-28 22:57:54 +0700

    Lập trình cấu trúc dữ liệu Stack, Queue và Cây

---
+++ Queue/queue.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Cấu trúc hàng đợi có những chức năng sau:
+        - EnQueue: Thêm phần tử vào cuối của Queue
+        - DeQueue: Xóa phần tử khỏi đầu của Queue. 
+        - IsEmpty: Kiểm tra xem Queue có rỗng không?
+        - Front: Lấy giá trị của phần tử đầu của Queue. lấy giá trị không làm thay đổi Queue.
+*/
+
+int main()
+{
+    return 0;
+}
---
+++ TreeStructure/TreeStructure.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+typedef struct Node
+{
+    int id;
+    Node* leftMostChild;
+    Node* rightSibling;
+
+};
+
+Node * root;
+
+
+
+
+int main(){
+   
+    return 0;
+}
